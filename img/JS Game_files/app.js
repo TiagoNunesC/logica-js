67f79c5f88bf707159a5d79480a7605ed41b1ccb
Tiago Nunes <tiagoo2001@yahoo.com.br>
2024-10-04 12:52:14 -0300

    Exercícios aula

--- img/JS Game_files/app.js
+++ img/JS Game_files/app.js
@@ -20,3 +20,131 @@
 
 // let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' 
 // alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+
+/*alert('Olá!');
+
+let nome = prompt('Insira o seu nome');
+
+alert(`Legal! seja muito bem vindo ${ nome }`)
+*/
+
+/*let contador = 1;
+
+while (contador < 11) {
+    console.log(contador);
+    contador++;
+}
+*/
+
+/*let contador = 10;
+
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
+*/
+
+/*alert('Programa de contagem regressiva');
+
+let numero = prompt('Digite o número inicial do contador');
+
+while (numero >= 0) {
+    console.log(numero);
+    numero--;
+}
+*/
+
+/*alert('Programa de contagem progressiva');
+
+let numero = prompt('Digite o número final do contador');
+let inicio = 0;
+
+while (inicio <= numero) {
+    console.log(inicio);
+    inicio++;
+}
+*/
+
+/*console.log('Seja bem vindo(a)!');
+
+let nome = 'Tiago';
+
+console.log(`Olá ${nome}`);
+*/
+
+/*alert('Seja bem vindo(a)!');
+
+let nome = 'Tiago';
+
+alert(`Olá ${nome}`);
+*/
+
+/*let resposta = prompt('Qual a linguagem de programação que você mais gosta?');
+
+console.log(`Sua resposta foi: ${resposta}`);
+*/
+
+// let valor1 = 1;
+// let valor2 = 2;
+
+// let resultado = valor1 + valor2;
+
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);
+
+// let valor1 = 17;
+// let valor2 = 7;
+
+// let resultado = valor1 - valor2;
+
+// console.log(`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}`);
+
+// let idade = prompt('Insira sua idade:');
+
+// if (idade <18) {
+//     console.log('Você ainda é menor de idade')
+// } 
+// if(idade >= 18) {
+//         console.log('Você já é maior de idade')
+// }    
+
+// let numero = prompt('Digite um número');
+
+// if (numero < 0) {
+//     console.log('Número negativo');
+// } else if (numero > 0) {
+//     console.log('Número positivo');
+// } else {
+//     console.log('O número é zero')
+// }
+
+// let number = 1;
+
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+
+// let nota = 7;
+
+// if (nota >= 7) {
+//     console.log('Aprovado');
+// } else {
+//     console.log('Reprovado');
+// }
+
+// let numeroAleatório = Math.random();
+
+// console.log(numeroAleatório);
+
+// let numeroAleatório = parseInt(Math.random() * 10) + 1;
+
+// console.log(numeroAleatório);
+
+// let numeroAleatório = parseInt(Math.random() * 1000) + 1;
+
+// console.log(numeroAleatório);
+
+
+
+
+
